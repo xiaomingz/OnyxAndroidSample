0a921bf04fe88de8276e0e8b9d26418ce6057103
wang.suicheng <wsc@onyx-international.com>
2020-04-03 16:02:28 +0800

    refactor(clock): adjust black/white style theme

--- app/simple/Clock/src/main/kotlin/com/simplemobiletools/clock/activities/SimpleActivity.kt
+++ app/simple/Clock/src/main/kotlin/com/simplemobiletools/clock/activities/SimpleActivity.kt
@@ -1,9 +1,26 @@
 package com.simplemobiletools.clock.activities
 
+import android.annotation.SuppressLint
+import android.os.Bundle
 import com.simplemobiletools.clock.R
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
+import com.simplemobiletools.commons.extensions.baseConfig
 
+@SuppressLint("Registered")
 open class SimpleActivity : BaseSimpleActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        useDynamicTheme = false
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!useDynamicTheme) {
+            updateActionbarColor(baseConfig.backgroundColor)
+        }
+    }
+
     override fun getAppIconIDs() = arrayListOf(
             R.mipmap.ic_launcher_red,
             R.mipmap.ic_launcher_pink,
