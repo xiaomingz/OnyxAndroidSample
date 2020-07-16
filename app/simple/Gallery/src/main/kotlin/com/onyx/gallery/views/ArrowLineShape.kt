2437fde5d8663a03f2e45687f989be52722807c1
leung-onyx <leung@onyx-international.com>
2020-07-16 10:46:18 +0800

    refactor(Gallery):clean up the code

--- app/simple/Gallery/src/main/kotlin/com/onyx/gallery/views/ArrowLineShape.kt
+++ app/simple/Gallery/src/main/kotlin/com/onyx/gallery/views/ArrowLineShape.kt
@@ -10,9 +10,10 @@ import com.onyx.gallery.utils.ExpandShapeFactory
  * Created by Leung 2020/7/14 18:55
  **/
 class ArrowLineShape : BaseLineShape() {
-    private val arrowSizeFactor = 4.5f
-    private val arrowTanAngle = 3.5 / 8
-    
+    private val arrowLenFactor = 4.5f
+    private val arrowHeight = 8
+    private val halfBottomLine = 3.5
+
     override fun getType(): Int = ExpandShapeFactory.SHAP_ARROW_LINE
 
     override fun render(renderContext: RenderContext) {
@@ -25,37 +26,35 @@ class ArrowLineShape : BaseLineShape() {
             transform(getRenderMatrix(renderContext))
         }
         canvas.drawPath(path, paint)
-        val arrowSize = paint.strokeWidth * arrowSizeFactor
-        drawArrows(canvas, paint, arrowSize, downPoint.x, downPoint.y, currentPoint.x, currentPoint.y)
+        val arrowLen = paint.strokeWidth * arrowLenFactor
+        drawArrows(canvas, paint, arrowLen, downPoint.x, downPoint.y, currentPoint.x, currentPoint.y)
     }
 
-    private fun drawArrows(canvas: Canvas, paint: Paint, arrowSize: Float, x1: Float, y1: Float, x2: Float, y2: Float) {
-        val awrad = Math.atan(arrowTanAngle)
-        val arrXY_1 = rotateVec(x2 - x1, y2 - y1, awrad, arrowSize.toDouble())
-        val arrXY_2 = rotateVec(x2 - x1, y2 - y1, -awrad, arrowSize.toDouble())
+    private fun drawArrows(canvas: Canvas, paint: Paint, arrowLen: Float, x1: Float, y1: Float, x2: Float, y2: Float) {
+        val awrad = Math.atan(halfBottomLine / arrowHeight)
+        val arrXY_3 = rotateVector(x2 - x1, y2 - y1, awrad, arrowLen.toDouble())
+        val arrXY_4 = rotateVector(x2 - x1, y2 - y1, -awrad, arrowLen.toDouble())
 
-        val x3 = x2 - arrXY_1[0]
-        val y3 = y2 - arrXY_1[1]
+        val x3 = x2 - arrXY_3[0]
+        val y3 = y2 - arrXY_3[1]
 
-        val x4 = x2 - arrXY_2[0]
-        val y4 = y2 - arrXY_2[1]
+        val x4 = x2 - arrXY_4[0]
+        val y4 = y2 - arrXY_4[1]
 
         val arrowsPath = Path()
         arrowsPath.moveTo(x2, y2)
         arrowsPath.lineTo(x3, y3)
+        arrowsPath.moveTo(x2, y2)
         arrowsPath.lineTo(x4, y4)
-        arrowsPath.close()
-
-        canvas.drawLine(x3, y3, x2, y2, paint)
-        canvas.drawLine(x4, y4, x2, y2, paint)
+        canvas.drawPath(arrowsPath, paint)
     }
 
-    private fun rotateVec(px: Float, py: Float, angle: Double, arrowSize: Double): FloatArray {
+    private fun rotateVector(px: Float, py: Float, angle: Double, arrowLen: Double): FloatArray {
         var vx = px * Math.cos(angle) - py * Math.sin(angle)
         var vy = px * Math.sin(angle) + py * Math.cos(angle)
         val d = Math.sqrt(vx * vx + vy * vy)
-        vx = vx / d * arrowSize
-        vy = vy / d * arrowSize
+        vx = vx / d * arrowLen
+        vy = vy / d * arrowLen
         return floatArrayOf(vx.toFloat(), vy.toFloat())
     }
 
