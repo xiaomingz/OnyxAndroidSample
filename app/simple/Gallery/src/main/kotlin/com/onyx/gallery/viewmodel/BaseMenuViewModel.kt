7cad5267afe2ea693e772781bc9b223f035aabc8
leung-onyx <leung@onyx-international.com>
2020-07-17 15:58:09 +0800

    refactor(Gallery):mosaic menu add stroke width setting http://oa.o-in.me:9068/issues/62583

--- app/simple/Gallery/src/main/kotlin/com/onyx/gallery/viewmodel/BaseMenuViewModel.kt
+++ app/simple/Gallery/src/main/kotlin/com/onyx/gallery/viewmodel/BaseMenuViewModel.kt
@@ -68,8 +68,8 @@ open class BaseMenuViewModel : BaseViewModel() {
                 MenuAction.SCRIBBLE_CIRCLE -> ShapeFactory.SHAPE_CIRCLE
                 MenuAction.SCRIBBLE_RECTANGLE -> ShapeFactory.SHAPE_RECTANGLE
                 MenuAction.SCRIBBLE_DASH_LINE -> ExpandShapeFactory.SHAP_DASH_LINE
-                MenuAction.SCRIBBLE_WAVE_LINE-> ExpandShapeFactory.SHAP_WAVE_LINE
-                MenuAction.SCRIBBLE_ARROW_LINE-> ExpandShapeFactory.SHAP_ARROW_LINE
+                MenuAction.SCRIBBLE_WAVE_LINE -> ExpandShapeFactory.SHAP_WAVE_LINE
+                MenuAction.SCRIBBLE_ARROW_LINE -> ExpandShapeFactory.SHAP_ARROW_LINE
                 else -> defaultShape()
             }
 
--- app/simple/Gallery/src/main/kotlin/com/onyx/gallery/viewmodel/MosaicMenuViewModel.kt
+++ app/simple/Gallery/src/main/kotlin/com/onyx/gallery/viewmodel/MosaicMenuViewModel.kt
@@ -1,15 +1,49 @@
 package com.onyx.gallery.viewmodel
 
+import android.widget.SeekBar
+import androidx.lifecycle.MutableLiveData
 import com.onyx.gallery.handler.touch.TouchHandlerType
+import com.onyx.gallery.helpers.DrawArgs
+import com.onyx.gallery.models.MenuAction
 
 /**
  * Created by Leung on 2020/6/8
  */
 class MosaicMenuViewModel : BaseMenuViewModel() {
+    private val stepStrokeWidth = DrawArgs.stepStrokeWidth
+    val maxStrokeWidth = DrawArgs.maxStrokeWidth
+    val minStrokeWidth = DrawArgs.minStrokeWidth
+    val currStrokeWidth: MutableLiveData<Int> = MutableLiveData(DrawArgs.defaultStrokeWidth)
+    val onChangeListener: SeekBar.OnSeekBarChangeListener by lazy { initOnSeekBarChangeListener() }
 
     override fun updateTouchHandler() {
         globalEditBundle.touchHandlerManager.activateHandler(TouchHandlerType.MOSAIC)
         globalEditBundle.drawHandler.setRawDrawingRenderEnabled(false)
     }
 
+    private fun initOnSeekBarChangeListener(): SeekBar.OnSeekBarChangeListener = object : SeekBar.OnSeekBarChangeListener {
+        override fun onProgressChanged(seekBar: SeekBar, strokeWidth: Int, fromUser: Boolean) {
+            currStrokeWidth.value = strokeWidth
+            globalEditBundle.drawHandler.setStrokeWidth(strokeWidth.toFloat())
+        }
+
+        override fun onStartTrackingTouch(seekBar: SeekBar) {
+        }
+
+        override fun onStopTrackingTouch(seekBar: SeekBar) {
+        }
+    }
+
+    override fun onHandleMenu(action: MenuAction): Boolean {
+        when (action) {
+            MenuAction.STROKE_WIDTH_ADDITION -> onStrokeWidthAdd()
+            MenuAction.STROKE_WIDTH_SUBTRACTION -> onStrokeWidthSub()
+            else -> return super.onHandleMenu(action)
+        }
+        return true
+    }
+
+    private fun onStrokeWidthAdd() = currStrokeWidth.run { value = value?.plus(stepStrokeWidth)?.coerceAtMost(maxStrokeWidth) }
+
+    private fun onStrokeWidthSub() = currStrokeWidth.run { value = value?.minus(stepStrokeWidth)?.coerceAtLeast(minStrokeWidth) }
 }
