8a30e293e1f82f51862d594fdd24a2f54c83fb84
wang.suicheng <wsc@onyx-international.com>
2020-08-05 16:51:50 +0800

    refactor(VoiceRecoder): use new record path

--- app/simple/VoiceRecorder/src/main/kotlin/com/simplemobiletools/voicerecorder/extensions/Context.kt
+++ app/simple/VoiceRecorder/src/main/kotlin/com/simplemobiletools/voicerecorder/extensions/Context.kt
@@ -2,21 +2,19 @@ package com.simplemobiletools.voicerecorder.extensions
 
 import android.content.Context
 import com.simplemobiletools.commons.extensions.internalStoragePath
-import com.simplemobiletools.commons.helpers.isQPlus
-import com.simplemobiletools.voicerecorder.R
 import com.simplemobiletools.voicerecorder.helpers.Config
 import java.io.File
 
 val Context.config: Config get() = Config.newInstance(applicationContext)
+
+fun Context.getSaveRootPath(): String {
+    return internalStoragePath
+}
+
 fun Context.getDefaultSaveFolder(): File {
-    val folderPath = if (isQPlus()) {
-        cacheDir.absolutePath
-    } else {
-        val defaultFolder = File("$internalStoragePath/${getString(R.string.app_name)}")
-        if (!defaultFolder.exists()) {
-            defaultFolder.mkdir()
-        }
-        defaultFolder.absolutePath
+    val defaultFolder = File("${getSaveRootPath()}/VoiceRecord")
+    if (!defaultFolder.exists()) {
+        defaultFolder.mkdirs()
     }
-    return File(folderPath)
+    return defaultFolder
 }
