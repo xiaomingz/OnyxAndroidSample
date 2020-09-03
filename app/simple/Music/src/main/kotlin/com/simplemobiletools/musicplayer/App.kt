38637b8bcc45011fd5db81ca8ddb505e3106a30b
wang.suicheng <wsc@onyx-international.com>
2020-09-03 17:20:29 +0800

    refactor(Music): playList dialog use OnyxAlertDialog of kui style

--- app/simple/Music/src/main/kotlin/com/simplemobiletools/musicplayer/App.kt
+++ app/simple/Music/src/main/kotlin/com/simplemobiletools/musicplayer/App.kt
@@ -1,11 +1,13 @@
 package com.simplemobiletools.musicplayer
 
 import android.app.Application
+import com.onyx.android.sdk.utils.ResManager
 import com.simplemobiletools.commons.extensions.checkUseEnglish
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         checkUseEnglish()
+        ResManager.init(this)
     }
 }
